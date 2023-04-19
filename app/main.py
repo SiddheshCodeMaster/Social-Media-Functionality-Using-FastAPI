142e23601e69d6022059d826f114e823591934a7
Siddhesh Shankar <siddhesh.codemaster.github@gmail.com>
2023-04-19 15:52:06 +0530

    commit 2.3.1 Get post with specific id SQLAlchemy integration

--- app/main.py
+++ app/main.py
@@ -70,19 +70,25 @@ def get_posts(db: Session = Depends(get_db)):
     return {"data":posts}
 
 @app.get("/posts/{id}")
-def get_post(id: int, response: Response):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
-    test_post = cursor.fetchone()
-    if not test_post:
+def get_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
+    # test_post = cursor.fetchone()
+    posts = db.query(models.Post).filter(models.Post.id == id).first()
+    if not posts:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
-    return {"post_detail":test_post}
+    return {"post_detail":posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-                    (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_posts(post: Post,db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
+    #                 (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+    #print(**post.dict()) # unpack the dictionary
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"data":new_post}
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
