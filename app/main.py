dd39692bd41e16bb81be4697185eddb09d8adeb1
Siddhesh Shankar <siddhesh.codemaster.github@gmail.com>
2023-04-19 17:16:37 +0530

    Commit 3.0.1 UPDATE, DELETE posts with specific id with SQLALCHEMY Integrations

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 from fastapi import Depends, FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
@@ -11,16 +11,12 @@ from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
 
-from . import models
+from . import models, schemas
 from .database import engine
 from .database import get_db
 models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
 
 # Database connectivity with Postgre SQL:
 while True:
@@ -52,34 +48,29 @@ def find_index_post(id):
         if p['id'] == id:
             return i
 
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return {"data": posts}
-
 @app.get("/")
 def root():
     return {"message":"Welcome to the Social Media Functionality using FastAPI."}
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
     # print(posts)
-    return {"data":posts}
+    return posts
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
     # test_post = cursor.fetchone()
     posts = db.query(models.Post).filter(models.Post.id == id).first()
     if not posts:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
-    return {"post_detail":posts}
+    return posts
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post,db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(post: schemas.PostCreate,db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
     #                 (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
@@ -89,7 +80,7 @@ def create_posts(post: Post,db: Session = Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data":new_post}
+    return new_post
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
@@ -105,8 +96,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", 
     #                (post.title, post.content, post.published, str(id)))
     # updated_post = cursor.fetchone()
@@ -119,6 +110,6 @@ def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
     
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
-    return {'message':post_query.first()}
+    return post_query.first()
 
 
