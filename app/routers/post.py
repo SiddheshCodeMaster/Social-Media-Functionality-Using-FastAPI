11439f535d91472b200ab53b1aa88f1bb6af5d24
Siddhesh Shankar <siddhesh.codemaster.github@gmail.com>
2023-04-20 17:18:22 +0530

    Commit 2.9.4 Routers Connectivity and Simplifying the file paths

--- app/routers/post.py
+++ app/routers/post.py
@@ -8,9 +8,11 @@ from sqlalchemy.orm import Session
 from ..database import engine
 from ..database import get_db
 
-router = APIRouter()
+router = APIRouter(
+    prefix = "/posts"
+)
 
-@router.get("/posts", response_model=List[schemas.Post])
+@router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
@@ -18,7 +20,7 @@ def get_posts(db: Session = Depends(get_db)):
     # print(posts)
     return posts
 
-@router.get("/posts/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
     # test_post = cursor.fetchone()
@@ -27,7 +29,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
     return posts
 
-@router.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate,db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
     #                 (post.title, post.content, post.published))
@@ -40,7 +42,7 @@ def create_posts(post: schemas.PostCreate,db: Session = Depends(get_db)):
     db.refresh(new_post)
     return new_post
 
-@router.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
     
     # cursor.execute("""DELETE FROM posts WHERE id =  %s RETURNING *""", (str(id),))
@@ -54,7 +56,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@router.put("/posts/{id}", response_model=schemas.Post)
+@router.put("/{id}", response_model=schemas.Post)
 def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", 
     #                (post.title, post.content, post.published, str(id)))
