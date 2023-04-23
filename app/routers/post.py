7df110ebc9cc4017aa68f8527afe2437fc78025b
Siddhesh Shankar <siddhesh.codemaster.github@gmail.com>
2023-04-23 18:19:33 +0530

    Commit 3.0.6 DELETE and UPDATE posts by the owner it self Functionality

--- app/routers/post.py
+++ app/routers/post.py
@@ -54,11 +54,17 @@ def delete_post(id: int, db: Session = Depends(get_db),
     # cursor.execute("""DELETE FROM posts WHERE id =  %s RETURNING *""", (str(id),))
     # deleted_post = cursor.fetchone()
     # conn.commit()
-    deleted_post = db.query(models.Post).filter(models.Post.id == id)
+    post_query = db.query(models.Post).filter(models.Post.id == id)
     
-    if deleted_post.first() == None:
+    post = post_query.first()
+
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"post with id {id} does not exist.")
-    deleted_post.delete(synchronize_session=False)
+    
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code = status.HTTP_403_FORBIDDEN, detail = "Not Authorized to perform requested action")
+    
+    post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
@@ -71,10 +77,14 @@ def update_post(id: int, updated_post: schemas.PostCreate,
     # updated_post = cursor.fetchone()
     # conn.commit()
     post_query = db.query(models.Post).filter(models.Post.id == id)
+    
     post = post_query.first()
 
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"post with id {id} does not exist.")
+    
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code = status.HTTP_403_FORBIDDEN, detail = "Not Authorized to perform requested action")
     
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
