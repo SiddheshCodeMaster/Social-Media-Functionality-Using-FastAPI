de84e14c7176d42a4a0340aa8d7a9f147dd4f027
Siddhesh Shankar <siddhesh.codemaster.github@gmail.com>
2023-04-24 16:26:23 +0530

    Commit 3.0.9 Deletion of unnesscary code and libs

--- app/database.py
+++ app/database.py
@@ -1,3 +1,6 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -16,3 +19,19 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# Database connectivity with Postgre SQL:
+# while True:
+#     try:
+#         conn =psycopg2.connect(host='localhost',
+#                            database='fastapi', 
+#                            user='postgres', 
+#                            password='password1234',
+#                            cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was successfull.")
+#         break
+#     except Exception as error:
+#         print("Connecting to the database failed")
+#         print("Error: ", error)
+#         time.sleep(2)
--- app/main.py
+++ app/main.py
@@ -1,56 +1,13 @@
-
-from typing import Optional, List
 from fastapi import Depends, FastAPI
-from fastapi.params import Body
-from pydantic import BaseModel
 from random import randint, randrange
-from fastapi import Response 
-from fastapi import status
-from fastapi import HTTPException
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-from sqlalchemy.orm import Session
-
-from . import models, schemas, utils
+from . import models
 from .database import engine
-from .database import get_db
 
 from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
-
-# Database connectivity with Postgre SQL:
-while True:
-    try:
-        conn =psycopg2.connect(host='localhost',
-                           database='fastapi', 
-                           user='postgres', 
-                           password='password1234',
-                           cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was successfull.")
-        break
-    except Exception as error:
-        print("Connecting to the database failed")
-        print("Error: ", error)
-        time.sleep(2)
-
-
-my_posts = [{"title":"title of post 1","content":"content of post 1", "id":1}, 
-            {"title":"favorite foods", "content":"Pizza","id":2}]
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-    
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
         
 app.include_router(post.router)
 app.include_router(user.router)
