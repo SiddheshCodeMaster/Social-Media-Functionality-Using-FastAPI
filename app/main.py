b652b0e2c4b84529ad1175adcc52add116b63e3a
Siddhesh Shankar <siddhesh.codemaster.github@gmail.com>
2023-04-24 17:37:45 +0530

    Commit 3.1.0 Shift to environment variables

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-from fastapi import Depends, FastAPI
-from random import randint, randrange
+from fastapi import FastAPI
 from . import models
 from .database import engine
 
 from .routers import post, user, auth
+from .config import settings
 
 models.Base.metadata.create_all(bind = engine)
 
