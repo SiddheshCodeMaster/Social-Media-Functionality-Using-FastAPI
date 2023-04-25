7f92ee143c63770bb5d1d55f6fdad2f344c302f4
Siddhesh Shankar <siddhesh.codemaster.github@gmail.com>
2023-04-25 19:21:59 +0530

    Commit 3.1.2 VOTING System created

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, conint
 from datetime import datetime
 
 class PostBase(BaseModel):
@@ -40,3 +40,7 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[str] = None
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)
